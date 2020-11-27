0a0e41fbd9123c91924a1e2f30f8f5706569596f
kingjakeu <jakeyoo12@gmail.com>
2020-11-27 14:51:23 +0900

    GET 테스트용 생성

--- src/main/java/com/kingjakeu/springstudy/domain/member/controller/MemberController.java
+++ src/main/java/com/kingjakeu/springstudy/domain/member/controller/MemberController.java
@@ -4,10 +4,7 @@ import com.kingjakeu.springstudy.domain.member.dao.MemberRepository;
 import com.kingjakeu.springstudy.domain.member.dto.MemberSignUpRequestDto;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/member")
@@ -20,5 +17,10 @@ public class MemberController {
     @ApiOperation(value = "회원 가입")
     public void signUpMember(@RequestBody MemberSignUpRequestDto dto){
         this.memberRepository.save(dto.toEntity());
+    }
+
+    @GetMapping("/")
+    public String getMember(){
+        return "hello";
     }
 }
