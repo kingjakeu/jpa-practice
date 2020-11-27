29bc57923e063d5f85dec520377a1edc88ff4af3
kingjakeu <jakeyoo12@gmail.com>
2020-11-27 18:05:56 +0900

    로그 연습

--- src/main/java/com/kingjakeu/springstudy/SpringStudyApplication.java
+++ src/main/java/com/kingjakeu/springstudy/SpringStudyApplication.java
@@ -1,13 +1,20 @@
 package com.kingjakeu.springstudy;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class SpringStudyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringStudyApplication.class, args);
+        log.error("error");
+        log.warn("warn");
+        log.info("info");
+        log.debug("debug");
+        log.trace("trace");
     }
 
 }
